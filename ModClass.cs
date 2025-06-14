0dd0f38c0bb72114ad2948d3cef28f8204ce26aa
PrashantMohta <mohtaprashantrocks@gmail.com>
2025-06-15 01:35:28 +0700

    friend

--- ModClass.cs
+++ ModClass.cs
@@ -1,4 +1,7 @@
-﻿using Modding;
+﻿using All4Love.Resources;
+using HKMirror.Reflection.InstanceClasses;
+using Modding;
+using Satchel;
 using Satchel.BetterPreloads;
 using System;
 using System.Collections;
@@ -34,6 +37,39 @@ namespace PrideMonthMod
                 new NamedEnemy("MageBlob").ShouldMatch(Preloads.Mistake)
             };
             On.tk2dSprite.Awake += Tk2dSprite_Awake;
+            On.KnightHatchling.FixedUpdate += KnightHatchling_FixedUpdate;
+            On.WeaverlingEnemyList.OnTriggerEnter2D += WeaverlingEnemyList_OnTriggerEnter2D;
+            On.GrimmEnemyRange.OnTriggerEnter2D += GrimmEnemyRange_OnTriggerEnter2D;
+        }
+
+        private void GrimmEnemyRange_OnTriggerEnter2D(On.GrimmEnemyRange.orig_OnTriggerEnter2D orig, GrimmEnemyRange self, Collider2D otherCollider)
+        {
+            if (otherCollider.GetComponent<Friend>() == null)
+            {
+                orig(self, otherCollider);
+            }
+        }
+
+        private void WeaverlingEnemyList_OnTriggerEnter2D(On.WeaverlingEnemyList.orig_OnTriggerEnter2D orig, WeaverlingEnemyList self, Collider2D otherCollider)
+        {
+            if(otherCollider.GetComponent<Friend>() == null) { 
+                orig(self, otherCollider);
+            }
+        }
+
+        private void KnightHatchling_FixedUpdate(On.KnightHatchling.orig_FixedUpdate orig, KnightHatchling self)
+        {
+            try
+            {
+                var selfR = new KnightHatchlingR(self);
+                if (selfR.target != null && selfR.target.GetComponent<Friend>() != null)
+                {
+                    selfR.target = null;
+                    ReflectionHelper.SetField(self, "currentState", self.LastFrameState);
+                }
+            }
+            catch (Exception e) { }
+            orig(self);
         }
 
         private void Tk2dSprite_Awake(On.tk2dSprite.orig_Awake orig, tk2dSprite self)
@@ -44,7 +80,10 @@ namespace PrideMonthMod
 
         private void CheckAndApply(GameObject gameObject)
         {
-            items.Find(item => item.CheckAndApply(gameObject));
+           var item = items.Find(item => item.CheckAndApply(gameObject));
+            if (item != null) {
+                gameObject.GetAddComponent<Friend>();
+            }
         }
 
     }
